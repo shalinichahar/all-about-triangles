8ec56e6d495e9a116f44f9726041e5e334b13aa3
Shalini Chahar <70834279+shalinichahar@users.noreply.github.com>
2023-09-08 14:58:06 +0530

    Update istriangle.js

--- istriangle.js
+++ istriangle.js
@@ -3,16 +3,16 @@ const trianglebtn = document.querySelector('#btn');
 const outputD = document.querySelector("#output");
 
 function calculateSumOfAngles( angle1, angle2, angle3) {
-    const sumOfAngles = angle1 + angle2 + angle3;
+    const sum = angle1 + angle2 + angle3;
     //console.log(sumOfAngles);
-   return sumOfAngles;
+   return sum;
 }
 
 function isTriangle() {
      const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
     if(sumOfAngles === 180){
         outputD.innerText = "Yay, the angles form a triangle";
-    } if(sumOfAngles === 0){
+    }else if(sumOfAngles === 0){
         outputD.innerText = "Please input angles!"
     }
     else {
